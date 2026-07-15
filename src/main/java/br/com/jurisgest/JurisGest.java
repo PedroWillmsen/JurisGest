376097817b715ad1dc6b58d40517eb9db2893de8
Pedro Willsmen <pedro.faviero@edu.pucrs.br>
2026-07-15 13:31:49 -0300

    inicio do projeto jurisgest

---
+++ src/main/java/br/com/jurisgest/JurisGest.java
@@ -0,0 +1,10 @@
+package br.com.jurisgest;
+
+public class JurisGest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("jurisgest rodando");
+	}
+
+}
